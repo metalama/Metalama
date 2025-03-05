765b58fd03e1d9d20f9a0e096fd15ddb41422dcf
Daniel Balas <daniel@postsharp.net>
2025-03-05 11:13:49 +0100

    Remove a file.

--- tests/Metalama.Framework.Tests.AspectTests/Tests/Aspects/Bugs/Bug35772.Dependency.cs
+++
@@ -1,7 +0,0 @@
-using Metalama.Framework.Aspects;
-using Metalama.Framework.Code;
-using System;
-
-namespace Metalama.Framework.Tests.Integration.Tests.Aspects.Bugs.Bug35772;
-
-#pragma warning disable CS0169
