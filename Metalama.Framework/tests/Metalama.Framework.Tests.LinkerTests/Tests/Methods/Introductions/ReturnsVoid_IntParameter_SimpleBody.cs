14438db3071748f40be145ff905e3caca50bb724
Daniel Balas <daniel@postsharp.net>
2025-03-10 11:30:34 +0100

    #35747 Update file headers in source files.

--- Metalama.Framework/tests/Metalama.Framework.Tests.LinkerTests/Tests/Methods/Introductions/ReturnsVoid_IntParameter_SimpleBody.cs
+++ Metalama.Framework/tests/Metalama.Framework.Tests.LinkerTests/Tests/Methods/Introductions/ReturnsVoid_IntParameter_SimpleBody.cs
@@ -1,4 +1,9 @@
-﻿namespace Metalama.Framework.Tests.LinkerTests.Tests.Methods.Introductions.ReturnsVoid_IntParameter_SimpleBody
+﻿// Copyright (c) 2020-2025 SharpCrafters s.r.o. and contributors.
+// SharpCrafters s.r.o. licenses this file to you under either an MIT license
+// or a proprietary license, depending on the repository from which it was obtained.
+// Refer to LICENSE.md in the repository root for complete details.
+
+namespace Metalama.Framework.Tests.LinkerTests.Tests.Methods.Introductions.ReturnsVoid_IntParameter_SimpleBody
 {
     // <target>
     class Target
