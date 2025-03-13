607e42972eae194c5d24ab4635e8cb923343fb2f
Daniel Balas <daniel@postsharp.net>
2025-03-13 13:34:59 +0100

    #35784 Added license notices to remaining source files.

--- Metalama.Framework/src/tests/Deprecated/Metalama.Reactive.UnitTests/SourceType.cs
+++ Metalama.Framework/src/tests/Deprecated/Metalama.Reactive.UnitTests/SourceType.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Immutable;
+﻿// Copyright (c) 2020-2025 SharpCrafters s.r.o. and contributors.
+// SharpCrafters s.r.o. licenses this file to you under either the MIT license or a proprietary license, depending on the repository from which it was obtained.
+// Refer to LICENSE.md in the repository root for complete details.
+
+using System.Collections.Immutable;
 using Metalama.Reactive.Sources;
 
 namespace Metalama.Reactive.UnitTests
--- Metalama.Framework/src/tests/Metalama.Framework.Tests.TemplateTests/Tests/Syntax/ForEachTests/ForEachRunTimeContainsProceed.cs
+++ Metalama.Framework/src/tests/Metalama.Framework.Tests.TemplateTests/Tests/Syntax/ForEachTests/ForEachRunTimeContainsProceed.cs
@@ -1,4 +1,8 @@
-#pragma warning disable CS8600, CS8603
+﻿// Copyright (c) 2020-2025 SharpCrafters s.r.o. and contributors.
+// SharpCrafters s.r.o. licenses this file to you under either the MIT license or a proprietary license, depending on the repository from which it was obtained.
+// Refer to LICENSE.md in the repository root for complete details.
+
+agma warning disable CS8600, CS8603
 using System;
 using System.Linq;
 using Metalama.Framework.Advising;
