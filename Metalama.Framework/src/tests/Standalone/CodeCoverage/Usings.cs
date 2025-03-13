f2dbf640a4cd84dc5b5365e5e5d6eb93818dad0c
Daniel Balas <daniel@postsharp.net>
2025-03-13 17:00:39 +0100

    #35784 More script fixes.

--- Metalama.Framework/src/tests/Standalone/CodeCoverage/Usings.cs
+++ Metalama.Framework/src/tests/Standalone/CodeCoverage/Usings.cs
@@ -2,7 +2,4 @@
 // SharpCrafters s.r.o. licenses this file to you under either the MIT license or a proprietary license, depending on the repository from which it was obtained.
 // Refer to LICENSE.md in the repository root for complete details.
 
-Copyright (c) SharpCrafters s.r.o. All rights reserved.
-// This project is not open source. Please see the LICENSE.md file in the repository root for details.
-
 global using Xunit;
