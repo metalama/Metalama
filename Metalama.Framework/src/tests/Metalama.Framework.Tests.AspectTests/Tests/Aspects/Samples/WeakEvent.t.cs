608bd02c16d2549f2e7bfb4459ba16248d77fa77
Gael Fraiteur <gael@postsharp.net>
2025-10-02 11:45:23 +0200

    Accepting test results.

--- Metalama.Framework/src/tests/Metalama.Framework.Tests.AspectTests/Tests/Aspects/Samples/WeakEvent.t.cs
+++ Metalama.Framework/src/tests/Metalama.Framework.Tests.AspectTests/Tests/Aspects/Samples/WeakEvent.t.cs
@@ -2,7 +2,7 @@ internal class TargetCode
 {
   private static readonly ActionEventBrokerCallbacks<EventHandler<EventArgs>, (object? , EventArgs)> EventFieldBrokerCallbacks_0 = new ActionEventBrokerCallbacks<EventHandler<EventArgs>, (object? , EventArgs)>(static (handler, me, args) => ((TargetCode)me).EventField_Invoke_WeakEvent(handler, args), static b => (sender, e) => b.Invoke((sender, e)), static (handler, me) => ((TargetCode)me).EventField_WeakEvent += handler, static (handler, me) => ((TargetCode)me).EventField_WeakEvent -= handler);
   private static readonly ActionEventBrokerCallbacks<EventHandler<EventArgs>, (object? , EventArgs)> EventBrokerCallbacks_0 = new ActionEventBrokerCallbacks<EventHandler<EventArgs>, (object? , EventArgs)>(static (handler, me, args) => ((TargetCode)me).Event_Invoke_WeakEvent(handler, args), static b => (sender, e) => b.Invoke((sender, e)), static (handler, me) => ((TargetCode)me).Event_WeakEvent += handler, static (handler, me) => ((TargetCode)me).Event_WeakEvent -= handler);
-  private List<EventHandler<EventArgs>> _delegates = new List<EventHandler<EventArgs>>();
+  private List<EventHandler<EventArgs>> _delegates = new();
   private event EventHandler<EventArgs> _eventField = default !;
   private volatile ActionEventBroker<EventHandler<EventArgs>, (object? , EventArgs)>? _eventFieldBroker;
   [WeakEvent]
