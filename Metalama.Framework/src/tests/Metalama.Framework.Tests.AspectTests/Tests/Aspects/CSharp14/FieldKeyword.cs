cf0861898b1c886bceb40271feb709febc515f92
Gael Fraiteur <gael@postsharp.net>
2025-10-08 16:41:02 +0200

    #1105 Unsupported language features in templates: field keyword, null-conditional assignments.

---
+++ Metalama.Framework/src/tests/Metalama.Framework.Tests.AspectTests/Tests/Aspects/CSharp14/FieldKeyword.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) 2020-2025 SharpCrafters s.r.o. and contributors.
+// SharpCrafters s.r.o. licenses this file to you under either the MIT license or a proprietary license, depending on the repository from which it was obtained.
+// Refer to LICENSE.md in the repository root for complete details.
+
+#if TEST_OPTIONS
+// @RequiredConstant(ROSLYN_5_0_0_OR_GREATER)
+#endif
+
+#if ROSLYN_5_0_0_OR_GREATER
+
+using Metalama.Framework.Aspects;
+using Metalama.Framework.Engine.Templating;
+
+namespace Metalama.Framework.Tests.AspectTests.Tests.Aspects.CSharp13.FieldKeyword;
+
+internal class TheAspect : TypeAspect
+{
+    [Introduce]
+    public string? Property
+    {
+        get => field;
+        set
+        {
+            field = value;
+        }
+    }
+}
+
+[TheAspect]
+internal class C;
+
+#endif
