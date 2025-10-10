759c2a9d48e72d51a9d78a01d6f5c2c73e13e900
Gael Fraiteur <gael@postsharp.net>
2025-10-10 12:33:02 +0200

    Disabling compound operator tests on .NET 4.8.

--- Metalama.Framework/src/tests/Metalama.Framework.Tests.UnitTests/CodeModel/CodeModelTests.CSharp14.cs
+++ Metalama.Framework/src/tests/Metalama.Framework.Tests.UnitTests/CodeModel/CodeModelTests.CSharp14.cs
@@ -2,7 +2,9 @@
 // SharpCrafters s.r.o. licenses this file to you under either the MIT license or a proprietary license, depending on the repository from which it was obtained.
 // Refer to LICENSE.md in the repository root for complete details.
 
-#if ROSLYN_5_0_0_OR_GREATER
+#if ROSLYN_5_0_0_OR_GREATER && NET7_0_OR_GREATER
+
+// We don't run these tests with old frameworks because they require the type CompilerFeatureRequiredAttribute.
 
 using Metalama.Framework.Code;
 using System.Linq;
