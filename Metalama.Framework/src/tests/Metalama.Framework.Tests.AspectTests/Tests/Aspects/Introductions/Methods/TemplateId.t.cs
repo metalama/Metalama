6b9d2bdbad2d797e616f3ea1b3cca8f4e5b31c57
Gael Fraiteur <gael@postsharp.net>
2025-12-03 11:02:52 +0100

    Add aspect test for TemplateAttribute.Id (#1210)

---
+++ Metalama.Framework/src/tests/Metalama.Framework.Tests.AspectTests/Tests/Aspects/Introductions/Methods/TemplateId.t.cs
@@ -0,0 +1,12 @@
+[Introduction]
+internal class TargetClass
+{
+  public void IntroducedByMethodId()
+  {
+    global::System.Console.WriteLine("Method template called.");
+  }
+  public global::System.Int32 IntroducedByPropertyId()
+  {
+    return (global::System.Int32)42;
+  }
+}
