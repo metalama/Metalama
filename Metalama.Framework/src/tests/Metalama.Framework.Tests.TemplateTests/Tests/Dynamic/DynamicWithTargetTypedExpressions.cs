ed7f5db8995586d93465ef03d25a60fd46e187b2
Gael Fraiteur <gael@postsharp.net>
2026-01-07 18:01:02 +0100

    Fix warnings in test file (#798)
    Add pragma warning disable directives for intentional test cases that trigger compiler warnings.

--- Metalama.Framework/src/tests/Metalama.Framework.Tests.TemplateTests/Tests/Dynamic/DynamicWithTargetTypedExpressions.cs
+++ Metalama.Framework/src/tests/Metalama.Framework.Tests.TemplateTests/Tests/Dynamic/DynamicWithTargetTypedExpressions.cs
@@ -5,6 +5,10 @@
 using Metalama.Framework.Aspects;
 using Metalama.Framework.Engine.Templating;
 
+#pragma warning disable CS0219 // Variable is assigned but its value is never used
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type
+#pragma warning disable IDE0047 // Remove unnecessary parentheses
+
 namespace Metalama.Framework.Tests.TemplateTests.Tests.Dynamic.DynamicWithTargetTypedExpressions;
 
 [CompileTime]
