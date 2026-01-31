9bf57a945b43a3fe0d36bb8e3802759616de689a
Gael Fraiteur <gael@postsharp.net>
2026-01-31 11:46:33 +0100

    Update eng.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -21,7 +21,6 @@ var product = new Product( MetalamaDependencies.Metalama )
 {
     OverriddenBuildAgentRequirements = new ContainerRequirements( ContainerHostKind.Windows )
     {
-        Memory = 16,
         Components =
         [
             // Must match global.json.
