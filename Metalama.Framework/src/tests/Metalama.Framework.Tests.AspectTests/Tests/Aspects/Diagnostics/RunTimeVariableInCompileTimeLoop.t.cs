a329b3d3caa1f6b21665a639274ec9faf2e07c03
Gael Fraiteur <gael@postsharp.net>
2026-02-06 14:30:48 +0100

    Add failing regression test for run-time variable in compile-time loop (#829)

---
+++ Metalama.Framework/src/tests/Metalama.Framework.Tests.AspectTests/Tests/Aspects/Diagnostics/RunTimeVariableInCompileTimeLoop.t.cs
@@ -0,0 +1,2 @@
+// CompileTimeAspectPipeline.ExecuteAsync failed.
+// Error LAMA0XXX on `stringBuilder.Append( ", " )`: `Cannot call a compile-time method with side effects from a run-time conditional block inside a compile-time loop.`
