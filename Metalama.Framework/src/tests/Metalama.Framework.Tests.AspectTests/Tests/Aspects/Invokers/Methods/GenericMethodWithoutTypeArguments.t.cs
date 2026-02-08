963bb05aecc68e13dc494097202ec6233228a44c
PostSharpBot <hello+agent@postsharp.net>
2026-02-08 22:12:21 +0100

    Add failing regression tests for generic method invoker (#765)

---
+++ Metalama.Framework/src/tests/Metalama.Framework.Tests.AspectTests/Tests/Aspects/Invokers/Methods/GenericMethodWithoutTypeArguments.t.cs
@@ -0,0 +1,12 @@
+public class TargetClass
+{
+  [TestAspect]
+  public void Foo()
+  {
+    this.Bar<global::System.Int32>(new global::Metalama.Framework.Tests.AspectTests.Tests.Aspects.Invokers.Methods.GenericMethodWithoutTypeArguments.TestData<global::System.Int32>());
+    return;
+  }
+  public void Bar<T>(TestData<T> data)
+  {
+  }
+}
