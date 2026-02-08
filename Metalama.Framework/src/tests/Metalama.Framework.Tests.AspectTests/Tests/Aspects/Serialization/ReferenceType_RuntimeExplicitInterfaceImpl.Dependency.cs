f1377daad838e59066d0bbfa53dd7fce597014fc
PostSharpBot <hello+agent@postsharp.net>
2026-02-08 23:00:14 +0100

    Add regression tests for runtime interface on RunTimeOrCompileTime type (#758)

---
+++ Metalama.Framework/src/tests/Metalama.Framework.Tests.AspectTests/Tests/Aspects/Serialization/ReferenceType_RuntimeExplicitInterfaceImpl.Dependency.cs
@@ -0,0 +1,58 @@
+// Copyright (c) 2020-2025 SharpCrafters s.r.o. and contributors.
+// SharpCrafters s.r.o. licenses this file to you under either the MIT license or a proprietary license, depending on the repository from which it was obtained.
+// Refer to LICENSE.md in the repository root for complete details.
+
+using Metalama.Framework.Aspects;
+using Metalama.Framework.Serialization;
+using System;
+
+namespace Metalama.Framework.Tests.AspectTests.Tests.Aspects.Serialization.ReferenceType_RuntimeExplicitInterfaceImpl;
+
+[RunTime]
+public interface IFace
+{
+    int Value { get; set; }
+
+    int ValueGetOnly { get; }
+}
+
+[RunTimeOrCompileTime]
+public class ReferenceType : IFace, ICompileTimeSerializable
+{
+    // Implicit member (not part of explicit interface impl).
+    public int OwnValue { get; set; }
+
+    // Explicit interface implementations.
+    int IFace.Value { get; set; }
+
+    int IFace.ValueGetOnly => OwnValue;
+
+    public ReferenceType( int value )
+    {
+        OwnValue = value;
+    }
+}
+
+[Inheritable]
+public class TestAspect : OverrideMethodAspect
+{
+    public ReferenceType SerializedValue;
+
+    public TestAspect( int x )
+    {
+        SerializedValue = new ReferenceType( x );
+    }
+
+    public override dynamic? OverrideMethod()
+    {
+        Console.WriteLine( meta.CompileTime( SerializedValue.OwnValue ) );
+
+        return meta.Proceed();
+    }
+}
+
+public class BaseClass
+{
+    [TestAspect( 42 )]
+    public virtual void Foo() { }
+}
