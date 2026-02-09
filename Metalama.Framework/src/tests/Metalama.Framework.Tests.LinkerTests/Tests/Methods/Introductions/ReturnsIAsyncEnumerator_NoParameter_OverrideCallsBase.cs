5b6e017be0d90bcd700dcfc3aac4035cf18ca5ae
PostSharpBot <hello+agent@postsharp.net>
2026-02-09 23:55:10 +0100

    Wrap @AssemblyReference directives in #if TEST_OPTIONS (#840)

--- Metalama.Framework/src/tests/Metalama.Framework.Tests.LinkerTests/Tests/Methods/Introductions/ReturnsIAsyncEnumerator_NoParameter_OverrideCallsBase.cs
+++ Metalama.Framework/src/tests/Metalama.Framework.Tests.LinkerTests/Tests/Methods/Introductions/ReturnsIAsyncEnumerator_NoParameter_OverrideCallsBase.cs
@@ -2,7 +2,9 @@
 // SharpCrafters s.r.o. licenses this file to you under either the MIT license or a proprietary license, depending on the repository from which it was obtained.
 // Refer to LICENSE.md in the repository root for complete details.
 
+#if TEST_OPTIONS
 // @AssemblyReference(System.Linq.AsyncEnumerable)
+#endif
 using System;
 using System.Collections.Generic;
 using static Metalama.Framework.Tests.LinkerTests.Tests.Api;
