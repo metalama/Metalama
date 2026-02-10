a991b72fca185157a7af04baa37aed940ac56641
PostSharpBot <hello+agent@postsharp.net>
2026-02-10 10:27:05 +0100

    Skip async enumerable linker tests on net48 (#840)

--- Metalama.Framework/src/tests/Metalama.Framework.Tests.LinkerTests/Tests/Methods/Introductions/ReturnsIAsyncEnumerator_NoParameter_OverrideCallsBase.cs
+++ Metalama.Framework/src/tests/Metalama.Framework.Tests.LinkerTests/Tests/Methods/Introductions/ReturnsIAsyncEnumerator_NoParameter_OverrideCallsBase.cs
@@ -2,6 +2,9 @@
 // SharpCrafters s.r.o. licenses this file to you under either the MIT license or a proprietary license, depending on the repository from which it was obtained.
 // Refer to LICENSE.md in the repository root for complete details.
 
+#if TEST_OPTIONS
+// @RequiredConstant(NET5_0_OR_GREATER)
+#endif
 using System;
 using System.Collections.Generic;
 using static Metalama.Framework.Tests.LinkerTests.Tests.Api;
