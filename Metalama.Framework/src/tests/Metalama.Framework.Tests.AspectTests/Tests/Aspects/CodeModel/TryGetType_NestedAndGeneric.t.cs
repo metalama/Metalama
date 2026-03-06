0f45fea60ae0bb8a9b6c5c5441eacdb3c18afecd
PostSharpBot <hello+agent@postsharp.net>
2026-03-06 11:15:06 +0100

    Add additional TryGetType tests for nested and generic types (#782)

---
+++ Metalama.Framework/src/tests/Metalama.Framework.Tests.AspectTests/Tests/Aspects/CodeModel/TryGetType_NestedAndGeneric.t.cs
@@ -0,0 +1,7 @@
+// Warning MY001 on `TargetClass`: `Correctly returned false for non-existent nested type`
+// Warning MY001 on `TargetClass`: `Found generic type: System.Collections.Generic.List`
+// Warning MY001 on `TargetClass`: `Found nested type: System.Environment.SpecialFolder`
+[TestAspect]
+internal class TargetClass
+{
+}
