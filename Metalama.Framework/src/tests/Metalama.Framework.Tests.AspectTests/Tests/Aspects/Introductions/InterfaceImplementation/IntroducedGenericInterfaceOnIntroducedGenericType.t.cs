2c059c70c9fed00221b25fe5deb0adf2eea44927
PostSharpBot <hello+agent@postsharp.net>
2026-03-08 16:46:30 +0100

    Fix implementing introduced generic interface on introduced generic type (#598)
    Use definition ref instead of potentially-constructed ref when looking up
    interface implementation collections in IntroducedNamedType, matching the
    pattern used by SourceNamedTypeImpl.

---
+++ Metalama.Framework/src/tests/Metalama.Framework.Tests.AspectTests/Tests/Aspects/Introductions/InterfaceImplementation/IntroducedGenericInterfaceOnIntroducedGenericType.t.cs
@@ -0,0 +1,10 @@
+[IntroductionAttribute]
+public class TargetType
+{
+  interface ITest<T>
+  {
+  }
+  class Test<U> : global::Metalama.Framework.Tests.AspectTests.Tests.Aspects.Introductions.InterfaceImplementation.IntroducedGenericInterfaceOnIntroducedGenericType.TargetType.ITest<U>
+  {
+  }
+}
