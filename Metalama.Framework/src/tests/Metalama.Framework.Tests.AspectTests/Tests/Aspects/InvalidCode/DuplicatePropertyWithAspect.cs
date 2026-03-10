45e5d80e2a2b07cd304ddfb93124660cbc4285d9
PostSharpBot <hello+agent@postsharp.net>
2026-03-10 18:39:22 +0100

    Unskip DuplicatePropertyWithAspect test (#1056)
    Remove @Skipped directive to reproduce the linker crash when
    duplicate properties both have an aspect applied.

--- Metalama.Framework/src/tests/Metalama.Framework.Tests.AspectTests/Tests/Aspects/InvalidCode/DuplicatePropertyWithAspect.cs
+++ Metalama.Framework/src/tests/Metalama.Framework.Tests.AspectTests/Tests/Aspects/InvalidCode/DuplicatePropertyWithAspect.cs
@@ -2,10 +2,6 @@
 // SharpCrafters s.r.o. licenses this file to you under either the MIT license or a proprietary license, depending on the repository from which it was obtained.
 // Refer to LICENSE.md in the repository root for complete details.
 
-#if TESTOPTIONS
-// @Skipped(#1056)
-#endif
-
 using Metalama.Framework.Aspects;
 using System;
 
