0ddca578dbf8c57b93b027ff54319103e56cbeaf
PostSharpBot <hello+agent@postsharp.net>
2026-03-10 11:25:18 +0100

    Fix tuple deconstruction in compile-time foreach (#643)
    Add VisitForEachVariableStatement to TemplateAnnotator to annotate
    DeclarationExpression nodes inside ForEachVariableStatement with
    compile-time scope. Add corresponding override in
    TemplateCompilerRewriter to handle the compile-time case.

--- Metalama.Framework/src/tests/Metalama.Framework.Tests.TemplateTests/Tests/Syntax/ForEachTests/ForEachTupleDeconstructionCodeModel.cs
+++ Metalama.Framework/src/tests/Metalama.Framework.Tests.TemplateTests/Tests/Syntax/ForEachTests/ForEachTupleDeconstructionCodeModel.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Metalama.Framework.Aspects;
 using Metalama.Framework.Code;
 using Metalama.Framework.Engine.Templating;
@@ -16,9 +17,8 @@ namespace Metalama.Framework.Tests.AspectTests.Templating.Syntax.ForEachTests.Fo
         [TestTemplate]
         private dynamic? Template()
         {
-            // Compile-time list of tuples containing code model elements, similar to the original issue
-            var pairs = meta.CompileTime(
-                meta.Target.Parameters.SelectAsArray( p => (p.Name, p.Type.ToDisplayString()) ) );
+            // Compile-time list of tuples built from code model elements
+            var pairs = meta.CompileTime( GetPairs() );
 
             foreach ( var (name, typeName) in pairs )
             {
@@ -28,6 +28,14 @@ namespace Metalama.Framework.Tests.AspectTests.Templating.Syntax.ForEachTests.Fo
             var result = meta.Proceed();
 
             return result;
+        }
+
+        [CompileTime]
+        private static List<(string Name, string TypeName)> GetPairs()
+        {
+#pragma warning disable CS0618 // Use SelectAsList or SelectAsArray - using Linq Select intentionally
+            return meta.Target.Parameters.Select( p => (p.Name, p.Type.ToDisplayString()) ).ToList();
+#pragma warning restore CS0618
         }
     }
 
