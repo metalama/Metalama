673201adb0550b90dbfe2119834f66190d10899a
PostSharpBot <hello+agent@postsharp.net>
2026-03-11 08:50:47 +0100

    Add regression test for delegate aspect eligibility (#692)

---
+++ Metalama.Framework/src/tests/Metalama.Framework.Tests.AspectTests/Tests/Aspects/Eligibility/DelegateParameter.t.cs
@@ -0,0 +1,3 @@
+// CompileTimeAspectPipeline.ExecuteAsync failed.
+// Error LAMA0037 on `MyAspect`: `The aspect 'MyAspect' cannot be applied to the parameter 'D.x' because the declaring member of 'D.x' must not be a delegate member.`
+// Error LAMA0037 on `MyAspect`: `The aspect 'MyAspect' cannot be applied to the return parameter of 'D' because the declaring member of the return parameter of 'D' must not be a delegate member.`
