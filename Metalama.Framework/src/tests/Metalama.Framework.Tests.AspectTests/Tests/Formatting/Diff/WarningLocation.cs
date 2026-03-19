35e1118200bde5248c8f4d5dab1f4311ecdd718f
PostSharpBot <hello+agent@postsharp.net>
2026-03-19 13:54:37 +0100

    Fix warnings in WarningLocation test (#668)
    Add copyright header and remove unused using System directive.
    Update expected HTML files accordingly.

--- Metalama.Framework/src/tests/Metalama.Framework.Tests.AspectTests/Tests/Formatting/Diff/WarningLocation.cs
+++ Metalama.Framework/src/tests/Metalama.Framework.Tests.AspectTests/Tests/Formatting/Diff/WarningLocation.cs
@@ -1,9 +1,12 @@
+// Copyright (c) 2020-2025 SharpCrafters s.r.o. and contributors.
+// SharpCrafters s.r.o. licenses this file to you under either the MIT license or a proprietary license, depending on the repository from which it was obtained.
+// Refer to LICENSE.md in the repository root for complete details.
+
 #if TEST_OPTIONS
 // @RequiredConstant(NET5_0_OR_GREATER)
 // @RequiredConstant(ROSLYN_4_8_0_OR_GREATER)
 #endif
 #pragma warning disable CS0067, CS8602, CS8603, CS0169
-using System;
 using Metalama.Framework.Aspects;
 
 namespace Metalama.Framework.Tests.AspectTests.Tests.Formatting.Diff.WarningLocation
