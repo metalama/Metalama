6c883089fea7bec2b89d2b4a4c88c23905ed357d
PostSharpBot <hello+agent@postsharp.net>
2026-03-20 13:29:29 +0100

    Add failing test for MakeGenericInstance on non-generic method (#583)

---
+++ Metalama.Framework/src/tests/Metalama.Framework.Tests.AspectTests/Tests/Aspects/CodeModel/MakeGenericInstanceNonGenericMethod.cs
@@ -0,0 +1,35 @@
+// Copyright (c) SharpCrafters s.r.o. and contributors.
+// SharpCrafters s.r.o. licenses this file to you under either the MIT license or a proprietary license, depending on the repository from which it was obtained.
+// Refer to LICENSE.md in the repository root for complete details.
+
+using System.Collections.Generic;
+using System.Linq;
+using Metalama.Framework.Advising;
+using Metalama.Framework.Aspects;
+using Metalama.Framework.Code;
+
+namespace Metalama.Framework.Tests.PublicPipeline.Aspects.CodeModel.MakeGenericInstanceNonGenericMethod;
+
+public class MyAspect : TypeAspect
+{
+    [Introduce]
+    public bool IntroducedEquals()
+    {
+        var equalityComparerEquals = ((INamedType) TypeFactory.GetType( typeof(EqualityComparer<>) ))
+            .Methods.OfName( "Equals" ).Single();
+
+        foreach (var member in meta.Target.Type.AllFieldsAndProperties)
+        {
+            var equals = equalityComparerEquals.MakeGenericInstance( member.Type );
+        }
+
+        return true;
+    }
+}
+
+// <target>
+[MyAspect]
+internal class Target
+{
+    private int _i;
+}
