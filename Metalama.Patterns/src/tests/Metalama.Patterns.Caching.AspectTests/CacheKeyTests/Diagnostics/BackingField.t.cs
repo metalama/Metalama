e48385265161c1fdab8b5bc6f5645dd8403daa8a
PostSharpBot <hello+agent@postsharp.net>
2026-03-20 13:16:38 +0100

    Report LAMA5112 diagnostic for [CacheKey] on property backing field (#591)

--- Metalama.Patterns/src/tests/Metalama.Patterns.Caching.AspectTests/CacheKeyTests/Diagnostics/BackingField.t.cs
+++ Metalama.Patterns/src/tests/Metalama.Patterns.Caching.AspectTests/CacheKeyTests/Diagnostics/BackingField.t.cs
@@ -1,1 +1,1 @@
-// Error LAMA5112 on `P`: `The [CacheKey] aspect cannot be applied to the backing field of property 'Target.P'. Apply the aspect directly to the property instead.`
+// Error LAMA5112 on `Target`: `The [CacheKey] aspect cannot be applied to the backing field of property 'Target.P.field'. Apply the aspect directly to the property instead.`
