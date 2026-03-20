214e3752075a0c297f1d9ad0a250687aec1215be
PostSharpBot <hello+agent@postsharp.net>
2026-03-20 16:48:03 +0100

    Fix standalone Issue1414 test: add using System and fix CA1822 (#1414)

--- Metalama.Framework/src/tests/Standalone/Issue1414/Class1.cs
+++ Metalama.Framework/src/tests/Standalone/Issue1414/Class1.cs
@@ -1,12 +1,18 @@
 // Standalone repro for issue #1414: Metalama fails when nuget.config
 // contains a relative path in fallbackPackageFolders.
 
+using System;
 using Metalama.Framework.Aspects;
 
 internal class Class1
 {
+    private int _counter;
+
     [Log]
-    public void DoWork() { }
+    public void DoWork()
+    {
+        _counter++;
+    }
 }
 
 internal class LogAttribute : OverrideMethodAspect
