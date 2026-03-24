0a4eda94dd380470e93f9ab6820bc17760b56d76
PostSharpBot <hello+agent@postsharp.net>
2026-03-24 17:50:19 +0100

    Add failing tests for System.Index and System.Range serialization (#1459)

---
+++ Metalama.Framework/src/tests/Metalama.Framework.Tests.UnitTests/SyntaxSerialization/IndexSerializerTests.cs
@@ -0,0 +1,39 @@
+// Copyright (c) 2020-2025 SharpCrafters s.r.o. and contributors.
+// SharpCrafters s.r.o. licenses this file to you under either the MIT license or a proprietary license, depending on the repository from which it was obtained.
+// Refer to LICENSE.md in the repository root for complete details.
+
+using System;
+using Xunit;
+
+namespace Metalama.Framework.Tests.UnitTests.SyntaxSerialization
+{
+    public sealed class IndexSerializerTests : SerializerTestsBase
+    {
+        [Fact]
+        public void TestIndexFromStart()
+        {
+            using var testContext = this.CreateSerializationTestContext( "" );
+
+            var index = new Index( 5 );
+            Assert.Equal( "new global::System.Index(5, false)", testContext.Serialize( index ).ToString() );
+        }
+
+        [Fact]
+        public void TestIndexFromEnd()
+        {
+            using var testContext = this.CreateSerializationTestContext( "" );
+
+            var index = new Index( 3, true );
+            Assert.Equal( "new global::System.Index(3, true)", testContext.Serialize( index ).ToString() );
+        }
+
+        [Fact]
+        public void TestIndexZero()
+        {
+            using var testContext = this.CreateSerializationTestContext( "" );
+
+            var index = new Index( 0 );
+            Assert.Equal( "new global::System.Index(0, false)", testContext.Serialize( index ).ToString() );
+        }
+    }
+}
---
+++ Metalama.Framework/src/tests/Metalama.Framework.Tests.UnitTests/SyntaxSerialization/RangeSerializerTests.cs
@@ -0,0 +1,36 @@
+// Copyright (c) 2020-2025 SharpCrafters s.r.o. and contributors.
+// SharpCrafters s.r.o. licenses this file to you under either the MIT license or a proprietary license, depending on the repository from which it was obtained.
+// Refer to LICENSE.md in the repository root for complete details.
+
+using System;
+using Xunit;
+
+namespace Metalama.Framework.Tests.UnitTests.SyntaxSerialization
+{
+    public sealed class RangeSerializerTests : SerializerTestsBase
+    {
+        [Fact]
+        public void TestRange()
+        {
+            using var testContext = this.CreateSerializationTestContext( "" );
+
+            var range = new Range( new Index( 1 ), new Index( 3 ) );
+
+            Assert.Equal(
+                "new global::System.Range(new global::System.Index(1, false), new global::System.Index(3, false))",
+                testContext.Serialize( range ).ToString() );
+        }
+
+        [Fact]
+        public void TestRangeWithFromEnd()
+        {
+            using var testContext = this.CreateSerializationTestContext( "" );
+
+            var range = new Range( new Index( 0 ), new Index( 1, true ) );
+
+            Assert.Equal(
+                "new global::System.Range(new global::System.Index(0, false), new global::System.Index(1, true))",
+                testContext.Serialize( range ).ToString() );
+        }
+    }
+}
