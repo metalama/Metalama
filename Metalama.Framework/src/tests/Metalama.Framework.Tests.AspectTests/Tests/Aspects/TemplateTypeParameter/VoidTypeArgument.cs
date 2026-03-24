05b6143671d0849d2f6cb69a4f1fde43e5cb61ac
PostSharpBot <hello+agent@postsharp.net>
2026-03-24 14:21:05 +0100

    Add regression test for void type argument (#789)

---
+++ Metalama.Framework/src/tests/Metalama.Framework.Tests.AspectTests/Tests/Aspects/TemplateTypeParameter/VoidTypeArgument.cs
@@ -0,0 +1,29 @@
+// Copyright (c) 2020-2025 SharpCrafters s.r.o. and contributors.
+// SharpCrafters s.r.o. licenses this file to you under either the MIT license or a proprietary license, depending on the repository from which it was obtained.
+// Refer to LICENSE.md in the repository root for complete details.
+
+using Metalama.Framework.Aspects;
+using Metalama.Framework.Code;
+
+namespace Metalama.Framework.Tests.AspectTests.Tests.Aspects.TemplateTypeParameters.VoidTypeArgument;
+
+public class Aspect : MethodAspect
+{
+    public override void BuildAspect( IAspectBuilder<IMethod> builder )
+    {
+        builder.Override( nameof(Template), args: new { T = typeof(void) } );
+    }
+
+    [Template]
+    private void Template<[CompileTime] T>()
+    {
+        T x;
+    }
+}
+
+// <target>
+public class TargetCode
+{
+    [Aspect]
+    public static void M() { }
+}
