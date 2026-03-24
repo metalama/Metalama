0c7f5a3b9d6c59439b36f0d18583bad6320a33d0
PostSharpBot <hello+agent@postsharp.net>
2026-03-24 08:25:56 +0100

    Add failing regression test for #757: advice from template should error

---
+++ Metalama.Framework/src/tests/Metalama.Framework.Tests.AspectTests/Tests/Aspects/Introductions/Fields/IntroduceFromTemplate.t.cs
@@ -0,0 +1,2 @@
+// CompileTimeAspectPipeline.ExecuteAsync failed.
+// Error LAMA0041 on `Method`: `The IAspectBuilder has been disposed and can no longer be used. Advice can only be added during the BuildAspect phase, not from templates.`
