6d20ddcd81a66731814f0447256a9368860c4f98
PostSharpBot <hello+agent@postsharp.net>
2026-03-27 08:39:11 +0100

    Add regression test for InsertParameter (#657)
    Add test that exercises the new InsertParameter API on IMethodBaseBuilder,
    which allows inserting parameters at a specific index position.

---
+++ Metalama.Framework/src/tests/Metalama.Framework.Tests.AspectTests/Tests/Aspects/Introductions/Methods/InsertParameter.t.cs
@@ -0,0 +1,8 @@
+[Introduction]
+internal class TargetClass
+{
+  public void IntroducedMethod(global::System.Int32 x, global::System.Int32 y = 42, global::System.String message = default)
+  {
+    global::System.Console.WriteLine(message);
+  }
+}
