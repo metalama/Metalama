6c7e3b0d589f314fe3c9773c140ee15193bdb8c3
PostSharpBot <hello+agent@postsharp.net>
2026-04-14 09:59:47 +0200

    Add failing test for forwarding constructor parameter discard (#1574)

---
+++ Metalama.Framework/src/tests/Metalama.Framework.Tests.AspectTests/Tests/Aspects/AppendParameter/Preserved_ChainedConstructor_Forward.t.cs
@@ -0,0 +1,22 @@
+[MyAspect]
+public class Order
+{
+  public Order(int id, [AspectGenerated] DateTime creationTime)
+  {
+    this.Id = id;
+  }
+  public Order(int id, string label, [AspectGenerated] DateTime creationTime) : this(id, creationTime)
+  {
+    this.Label = label;
+  }
+  public int Id { get; }
+  public string? Label { get; }
+  [SourceCompatibilityConstructor]
+  public Order(int id) : this(id: id, creationTime: DateTime.Now)
+  {
+  }
+  [SourceCompatibilityConstructor]
+  public Order(int id, string label) : this(id: id, label: label, creationTime: DateTime.Now)
+  {
+  }
+}
