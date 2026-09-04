9acf4e5f370a90e9c97301df1c311532874a7851
teamcity <teamcity@postsharp.net>
2026-09-04 05:26:13 -0700

    Name the code refactoring provider entry points by constants (#1930)

--- Metalama.Framework/src/Metalama.Framework.EditorExtensions/MetalamaCodeRefactoringProvider.cs
+++ Metalama.Framework/src/Metalama.Framework.EditorExtensions/MetalamaCodeRefactoringProvider.cs
@@ -2,6 +2,7 @@
 // SharpCrafters s.r.o. licenses this file to you under either the MIT license or a proprietary license, depending on the repository from which it was obtained.
 // Refer to LICENSE.md in the repository root for complete details.
 
+using Metalama.Framework.DesignTime;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeRefactorings;
 using System.Composition;
@@ -31,33 +32,32 @@ namespace Metalama.Framework.CompilerExtensions
                 case ProcessKind.RoslynCodeAnalysisService:
                 case ProcessKind.DevEnv:
                     ResourceExtractor.TryCreateInstance<CodeRefactoringProvider>(
-                        "Metalama.Framework.DesignTime",
-                        "Metalama.Framework.DesignTime.VisualStudio.CodeFixes.VsCodeRefactoringProvider",
+                        RoslynEntryPointTypeNames.DesignTimeAssemblyName,
+                        RoslynEntryPointTypeNames.VsCodeRefactoringProvider,
                         out this._impl );
 
                     break;
 
                 case ProcessKind.Rider:
                     ResourceExtractor.TryCreateInstance<CodeRefactoringProvider>(
-                        "Metalama.Framework.DesignTime",
-                        "Metalama.Framework.DesignTime.Rider.RiderCodeRefactoringProvider",
+                        RoslynEntryPointTypeNames.DesignTimeAssemblyName,
+                        RoslynEntryPointTypeNames.RiderCodeRefactoringProvider,
                         out this._impl );
 
                     break;
 
                 default:
                     ResourceExtractor.TryCreateInstance<CodeRefactoringProvider>(
-                        "Metalama.Framework.DesignTime",
-                        "Metalama.Framework.DesignTime.CodeFixes.TheCodeRefactoringProvider",
+                        RoslynEntryPointTypeNames.DesignTimeAssemblyName,
+                        RoslynEntryPointTypeNames.TheCodeRefactoringProvider,
                         out this._impl );
 
                     break;
             }
         }
 
-#pragma warning disable VSTHRD110
-        public override Task ComputeRefactoringsAsync( CodeRefactoringContext context )
 #pragma warning disable VSTHRD110 // Observe the awaitable result of this method call by awaiting it, assigning to a variable, or passing it to another method
+        public override Task ComputeRefactoringsAsync( CodeRefactoringContext context )
             => this._impl?.ComputeRefactoringsAsync( context ) ?? Task.CompletedTask;
 #pragma warning restore VSTHRD110
     }
